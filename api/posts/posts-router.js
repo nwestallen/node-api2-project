ccc8a74f096a3f2d73c8fc3d59cd271386a7ec73
Nick Allen <nwestallen@gmail.com>
2021-03-10 09:18:46 -0500

    GET /api/posts/:id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('./posts-model');
 
+router.use(express.json());
+
 //GET /api/posts
 router.get('/', (req, res) => {
     Post.find()
@@ -13,4 +15,19 @@ router.get('/', (req, res) => {
           res.status(500).json({ message: "The posts information could not be retrieved" });
       });
 });
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Post.findById(id)
+      .then(post => {
+          if(post) {
+              res.status(200).json(post);
+          } else {
+              res.status(404).json({ message: "The post with the specified ID does not exist" });
+          }
+      })
+      .catch(err => {
+          res.status(500).json({ message: "The post information could not be retrieved"});
+      });
+})
 module.exports = router;
