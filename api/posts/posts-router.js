f7f86d3ac26fb7dca020894bb084ec43da7a7f62
Nick Allen <nwestallen@gmail.com>
2021-03-09 20:30:10 -0500

    GET posts returns results

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const express = require('express');
+const router = express.Router();
+const Post = require('./posts-model');
+
+//GET /api/posts
+router.get('/', (req, res) => {
+    Post.find()
+      .then(posts => {
+          res.status(200).json(posts);
+      })
+      .catch(err => {
+          res.status(500).json({ message: "The posts information could not be retrieved" });
+      });
+});
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,5 +2,8 @@
 // require your posts router and connect it here
 const express = require('express');
 const server = express();
+const postRoutes = require('./posts/posts-router');
+
+server.use('/api/posts', postRoutes);
 
 module.exports = server;
