2f66e83bae72eb9195e257220c6389f436a28879
Nick Allen <nwestallen@gmail.com>
2021-03-10 11:26:16 -0500

    GET /api/posts/:id/comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -81,4 +81,23 @@ router.delete('/:id', (req,res) => {
       });
 });
 
+router.get('/:id/comments', (req,res) => {
+    const id = req.params.id;
+    Post.findById(id)
+      .then(post => {
+          if(post) {
+            Post.findPostComments(id)
+            .then(comments => {
+                res.status(200).json(comments);
+            })
+            .catch(err => {
+                res.status(500).json({ message: "The comments information could not be retrieved" });
+            });
+            }   else {
+            res.status(404).json({ message: "The post with the specified ID does not exist" });
+            }   
+        })
+      .catch(err => res.status(500).json({ message: "The post information could not be retrieved" }));
+});
+
 module.exports = router;
